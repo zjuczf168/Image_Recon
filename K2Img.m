32f54946e09f31dcda92d0e88b6273285f897e5c
Zhifeng Chen <93971001+zjuczf168@users.noreply.github.com>
2022-12-01 16:58:17 +1100

    Add files via upload

---
+++ K2Img.m
@@ -0,0 +1,10 @@
+function [Img]= K2Img(K)
+
+%  Img = ifft2(fftshift(K)); %all philips data, GE head
+% Img = fftshift(ifft2(K)); %
+% Img = fftshift(ifft2(fftshift(K)),2);
+% Img = fftshift(ifft2((K)),2);
+% Img = fftshift(fft2(fftshift(K))); %cAI, caridac, Shepp-Logan Phantom,Qu Peng
+Img = fftshift(ifft2(fftshift(K))); %cAI, caridac,Philips spine
+% Img = fftshift(ifft2(fftshift(K)),1);%3D data
+% % Img = fftshift(ifft2(ifftshift(K)));
